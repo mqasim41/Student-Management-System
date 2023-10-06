5c9ea304567602d3ff6af7c3d39790da7650d598
Ayesh Ahmad <ayeshahmad1222@gmail.com>
2023-10-06 19:23:15 +0500

    Update Main.java
    switch case

--- com/SMS/Main.java
+++ com/SMS/Main.java
@@ -17,70 +17,72 @@ public class Main {
             int option;
             // try catch to cater the exception thrown when the correct input not entered; on values other than 1,2,3,4
             try {  
-                option = sc.nextInt();  // ask the user for one of the four options
-                if (option == 4) {
-                    System.out.println("Program ended successfully");
-                    sms.writeToFile();
-                    System.out.println("Do you want to read the file? enter yes/no ");
-                    if ((sc.next().toLowerCase()).equals("yes")){
-                        sms.readFromFile();
-                        break;
-                    } else
-                        break;
-                }
-                // adding record
-                if (option == 1) {
-                    try {
-                        System.out.println("Which student you want to store in record? " +
-                                "\n*enter 1 for UG " +
-                                "\n*enter 2 for PG");     
-                        int input = sc.nextInt();
-                        if (input == 1) {
-                            try {
-                                System.out.println("Enter name, CMS, semester");
-                                boolean returnValue = sms.addRecord(sc.next(), sc.nextInt(), sc.nextInt(), 1);
-                                if (returnValue) {
-                                    System.out.println("UG Student added sucessfully");
-                                }
-                            } catch (OutOfMemoryException e) {   
-                                System.out.println(e);
-                            }
-                        } else if (input == 2) {
-                            try {
-                                System.out.println("Enter name, CMS, semester");
-                                boolean returnValue = sms.addRecord(sc.next(), sc.nextInt(), sc.nextInt(), 2);
-                                if (returnValue) {
-                                    System.out.println("PG Student added successfully");
-                                }
-                            } catch (OutOfMemoryException e) {   
-                                System.out.println(e);
-                            }
+                option = sc.nextInt();
+                switch (option) {
+                    // exit
+                    case 4:
+                        System.out.println("Program ended successfully");
+                        sms.writeToFile();
+                        System.out.println("Do you want to read the file? enter yes/no ");
+                        if ((sc.next().toLowerCase()).equals("yes")){
+                            sms.readFromFile();
+                            break;
+                        } else {
+                            break;
                         }
-                    } catch (Exception e) {    
-                        System.out.println(e);
-                    }
-                    // deleting the record
-                } else if (option == 2) {       
-                    try {
-                        System.out.println("Enter CMS ID for deleting the student");
-                        int id = sc.nextInt(); 
-                        boolean returnValue = sms.deleteStudent(id);
-                        if (returnValue) {
-                            System.out.println("Student deleted successfully at CMS ID " + id);
+                    // add student
+                    case 1:
+                        try {
+                            System.out.println("Which student you want to store in record? " +
+                                               "\n*enter 1 for UG " +
+                                               "\n*enter 2 for PG");     
+                            int input = sc.nextInt();
+                            if (input == 1) {
+                                try {
+                                    System.out.println("Enter name, CMS, semester");
+                                    boolean returnValue = sms.addRecord(sc.next(), sc.nextInt(), sc.nextInt(), 1);
+                                    if (returnValue) {
+                                        System.out.println("UG Student added sucessfully");
+                                    }
+                                } catch (OutOfMemoryException e) {   
+                                    System.out.println(e);
+                                }
+                            } else if (input == 2) {
+                                try {
+                                    System.out.println("Enter name, CMS, semester");
+                                    boolean returnValue = sms.addRecord(sc.next(), sc.nextInt(), sc.nextInt(), 2);
+                                    if (returnValue) {
+                                        System.out.println("PG Student added successfully");
+                                    }
+                                } catch (OutOfMemoryException e) {   
+                                    System.out.println(e);
+                                }
+                            }
+                        } catch (Exception e) {    
+                            System.out.println(e);
                         }
-                    } catch (StudentNotFoundException e) {   
-                        System.out.println(e);
+                    // delete student
+                    case 2:    
+                        try {
+                            System.out.println("Enter CMS ID for deleting the student");
+                            int id = sc.nextInt(); 
+                            boolean returnValue = sms.deleteStudent(id);
+                            if (returnValue) {
+                                System.out.println("Student deleted successfully at CMS ID " + id);
+                            }
+                        } catch (StudentNotFoundException e) {   
+                            System.out.println(e);
+                        }
+                    // search student
+                    case 3:    
+                        try {
+                            System.out.println("Enter CMS id for searching the student ");
+                            int id = sc.nextInt();  
+                            sms.searchStudent(id);
+                        } catch (StudentNotFoundException e) {
+                            System.out.println(e);
+                        }
                     }
-                    // search the student
-                } else if (option == 3) {      
-                    try {
-                        System.out.println("Enter CMS id for searching the student ");
-                        int id = sc.nextInt();  
-                        sms.searchStudent(id);
-                    } catch (StudentNotFoundException e) {
-                        System.out.println(e);
-                    }
-                }
             } catch (Exception e) {
                 System.out.println(e);
             }
