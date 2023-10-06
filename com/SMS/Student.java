adfdfd48cbba2148f9b67ba5fb684d13ad5fc8d6
Ayesh Ahmad <ayeshahmad1222@gmail.com>
2023-10-06 18:36:33 +0500

    Update Student.java

--- com/SMS/Student.java
+++ com/SMS/Student.java
@@ -3,20 +3,20 @@ package com.SMS;
 // Generalized Class for a Student
 public class Student {
     private String name;
-    private int cmsID;
-    Student(String name, int cmsID){
+    private int cmsId;
+    Student(String name, int cmsId){
         this.name = name;
-        this.cmsID = cmsID;
+        this.cmsId = cmsId;
     }
     public String getName(){return name;}
-    public int getCmsId(){return cmsID;}
+    public int getCmsId(){return cmsId;}
 }
 
 // UG Student
 class UGStudent extends Student{
     private int UGsem;
-    UGStudent(String name, int cmsID, int UGsem){
-        super(name, cmsID);
+    UGStudent(String name, int cmsId, int UGsem){
+        super(name, cmsId);
         this.UGsem = UGsem;
     }
     public int getUGsem(){return UGsem;}
@@ -25,8 +25,8 @@ class UGStudent extends Student{
 // PG Student
 class PGStudent extends Student{
     private int PGsem;
-    PGStudent(String name, int cmsID, int PGsem){
-        super(name, cmsID);
+    PGStudent(String name, int cmsId, int PGsem){
+        super(name, cmsId);
         this.PGsem = PGsem;
     }
     public int getPGsem(){return PGsem;}
